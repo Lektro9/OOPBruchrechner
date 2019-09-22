f7a6cffb3c29da986bc84c7e273768ad5f7ac138
Lektro <larskr93@gmail.com>
2019-09-22 19:44:44 +0200

    initialize first commit

---
+++ OOPBruchrechner_Kroll/Bruch.cs
@@ -0,0 +1,128 @@
+﻿//Autor:        Kroll
+//Datum:        18.09.2019
+//Dateiname:    Bruch.cs
+//Beschreibung: "Bauplan" für ein Objekt Bruch
+//Änderungen:
+//18.09.2019:   Entwicklungsbeginn  
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPBruchrechner_Kroll
+{
+    class Bruch
+    {
+        #region Eigenschaften
+        private int _Zaehler;
+        private int _Nenner;
+        private char _Vorzeichen;
+
+        #endregion
+
+        #region Accessoren/Modifier
+        public int Zaehler { get => _Zaehler; set => _Zaehler = value; }
+        public int Nenner { get => _Nenner; set => _Nenner = value; }
+        public char Vorzeichen { get => _Vorzeichen; set => _Vorzeichen = value; }
+        #endregion
+
+        #region Konstruktoren
+        public Bruch()
+        {
+            this.Zaehler = 1;
+            this.Nenner = 1;
+        }
+
+        public Bruch(int val1, int val2)
+        {
+            this.Zaehler = val1;
+            this.Nenner = val2;
+        }
+        #endregion
+
+        #region Worker
+        public Bruch Addieren(Bruch value)
+        {
+            int ergebnisZaehler = this.Zaehler * value.Nenner + this.Nenner * value.Zaehler;
+            int ergebnisNenner = this.Nenner * value.Nenner;
+            Bruch ergebnis = new Bruch(ergebnisZaehler, ergebnisNenner);
+            ergebnis.Kuerzen();
+            return ergebnis;
+        }
+
+        public Bruch Subtrahieren(Bruch value)
+        {
+            int ergebnisZaehler = this.Zaehler * value.Nenner - this.Nenner * value.Zaehler;
+            int ergebnisNenner = this.Nenner * value.Nenner;
+            Bruch ergebnis = new Bruch(ergebnisZaehler, ergebnisNenner);
+            ergebnis.Kuerzen();
+            return ergebnis;
+        }
+
+        public Bruch Multiplizieren(Bruch value)
+        {
+            int ergebnisZaehler = this.Zaehler * value.Zaehler;
+            int ergebnisNenner = this.Nenner * value.Nenner;
+
+            Bruch ergebnis = new Bruch(ergebnisZaehler, ergebnisNenner);
+            ergebnis.Kuerzen();
+            return ergebnis;
+        }
+
+        public Bruch Dividieren(Bruch value)
+        {
+            int ergebnisZaehler = this.Zaehler * value.Nenner;
+            int ergebnisNenner = this.Nenner * value.Zaehler;
+
+            Bruch ergebnis = new Bruch(ergebnisZaehler, ergebnisNenner);
+            ergebnis.Kuerzen();
+            return ergebnis;
+        }
+
+        private void Kuerzen()
+        {
+            //Euklidischer Alg.
+            int ggTZaehler = this.Zaehler;
+            int ggTNenner = this.Nenner;
+            int ggT;
+
+            if (ggTZaehler == 0)                    //ggT bestimmen
+            {
+                ggT = ggTNenner;
+            }
+            else
+            {
+                while (ggTNenner != 0)
+                {
+                    if (ggTZaehler > ggTNenner)
+                    {
+                        ggTZaehler = ggTZaehler - ggTNenner;
+                    }
+                    else
+                    {
+                        ggTNenner = ggTNenner - ggTZaehler;
+                    }
+                }
+                ggT = ggTZaehler;
+            }
+
+            this.Zaehler = this.Zaehler / ggT;
+            this.Nenner = this.Nenner / ggT;
+        }
+
+        public Bruch Zuweisung(Bruch value)
+        {
+            //Overloading ist für "=" nicht zulässig
+            this.Zaehler = value.Zaehler;
+            this.Nenner = value.Nenner;
+            Bruch neuerBruch = new Bruch(value.Zaehler, value.Nenner);
+            return neuerBruch;
+        }
+            #endregion
+
+            #region Schnittstellen
+            #endregion
+        }
+}
---
+++ OOPBruchrechner_Kroll/main.cs
@@ -0,0 +1,24 @@
+﻿//Autor:        Kroll
+//Datum:        18.09.2019
+//Dateiname:    main.cs
+//Beschreibung: Hauptfunktion
+//Änderungen:
+//18.09.2019:   Entwicklungsbeginn  
+using System;
+
+namespace OOPBruchrechner_Kroll
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Bruch b1 = new Bruch(4, 5);
+            Bruch b2 = new Bruch(1, 5);
+            Bruch bErgebnis = new Bruch(2, 3);
+            b1.Zaehler = 5;
+            bErgebnis.Zuweisung(b1.Subtrahieren(b2));
+
+            Console.WriteLine(bErgebnis.Zaehler + "/" + bErgebnis.Nenner);
+        }
+    }
+}
